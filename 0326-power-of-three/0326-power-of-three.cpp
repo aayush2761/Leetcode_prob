cbabc8cee4cdf882efaeecc99da6e50523c29175
aayush2761 <145021649+aayush2761@users.noreply.github.com>
2023-10-28 11:14:02 +0530

    Time: 25 ms (7.97%), Space: 6.2 MB (57.81%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     bool isPowerOfThree(int n) {
          if (n <= 0) {
@@ -12,16 +12,4 @@ public:
         return n == 1;
     }
     
-};*/
-
-class Solution {
-public:
-    bool isPowerOfThree(int n) {
-        if(n==1)
-        return 1;
-        if(n<0 || n==0 || n%3>0)
-        return 0;
-        return isPowerOfThree(n/3);
-
-    }
 };
