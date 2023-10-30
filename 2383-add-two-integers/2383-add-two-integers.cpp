73872f1da5e4aa094598efa58e1543ba892ea8f2
aayush2761 <145021649+aayush2761@users.noreply.github.com>
2023-10-30 23:18:36 +0530

    Time: 0 ms (100%), Space: 6.3 MB (25.04%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int  ans=0;
+        if((num1>=-100) && (num2<=100)) { 
+             ans=num1+num2;
+             }
+         return ans;
+    }
+};
