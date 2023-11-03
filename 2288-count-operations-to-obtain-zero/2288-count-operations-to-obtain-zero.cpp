c6e5259519dc5dac67044f358cb3b8bb4abe13fc
aayush2761 <145021649+aayush2761@users.noreply.github.com>
2023-11-03 17:40:24 +0530

    Time: 2 ms (55.54%), Space: 6.2 MB (55.54%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -6,13 +6,13 @@ public:
        if(num1>=num2){
            num1=num1-num2;
            ans++;
-           countOperations( num1, num2);
+           return countOperations( num1, num2);
        } 
        else{
            num2=num2-num1;
            ans++;
-           countOperations( num1, num2);
+           return countOperations( num1, num2);
        } 
-       return ans;
+       //
     }
 };
