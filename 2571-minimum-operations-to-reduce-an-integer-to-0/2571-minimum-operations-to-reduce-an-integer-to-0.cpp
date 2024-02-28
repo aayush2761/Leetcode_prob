b6957bcb1d332ae5f41a2c5350528ba50ca50e51
aayush2761 <145021649+aayush2761@users.noreply.github.com>
2024-02-28 19:01:13 +0530

    Time: 2 ms (54.78%), Space: 7.2 MB (32.65%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int ans=0;
+        while(n>0){
+            if(n&1==1){
+                ans++;
+                n/=2;
+                if(n&1==1){
+                    n++;
+
+                }
+               
+            }
+             else{
+                    n/=2;
+                }
+        }
+        return ans;
+    }
+};
