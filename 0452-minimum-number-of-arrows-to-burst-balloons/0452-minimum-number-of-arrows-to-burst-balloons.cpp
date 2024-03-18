132d92dce7ab3bf115f5772e659dc649c38d821f
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-03-18 17:48:54 +0530

    Time: 238 ms (91.88%), Space: 93.4 MB (41.78%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         if (points.empty()) return 0;
-        
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1]; // Sort by ending point
-        });
-        
+
+        sort(points.begin(), points.end());
+
         int arrows = 1;
         int end = points[0][1];
-        
+
         for (int i = 1; i < points.size(); i++) {
-            if (points[i][0] > end) {
+            if (points[i][0] <= end) {
+                // If current interval overlaps with previous, update end
+                end = min(end, points[i][1]);
+            } else {
+                // No overlap, need a new arrow
                 arrows++;
                 end = points[i][1];
             }
         }
-        
+
         return arrows;
     }
 };
