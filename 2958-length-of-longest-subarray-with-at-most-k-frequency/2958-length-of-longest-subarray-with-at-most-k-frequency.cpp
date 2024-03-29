f461c63e5d23f3e7f6016254ae0af9f3faf4faec
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-03-29 17:25:12 +0530

    Time: 210 ms (61.48%), Space: 148.3 MB (86.23%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,49 +1,46 @@
-// class Solution {
-// public:
-//     int maxSubarrayLength(vector<int>& nums, int k) {
-//         unordered_map<int,int>mp;
-//         int i=nums.size()-1,j=0;
-//         int len=0;
-//         while(i>=0){
-//             mp[nums[i]]++;
-//             int p=j;
-//             while(p<=i){
-//                if(mp[nums[p]]>k) {
-//                 len=max(len,i-j+1);
-//                 i--;
-//                 mp.clear();
-//                 break;
-//                }
-//                p++;
-//             }
-//             i--;
-//         }
-//        return len;
-        
-//     }
-// };
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int j=0,i=0;
         int ans=0;
-        //fo = first occurence
-        unordered_map<int,queue<int>>fo;//store index of nums
-        int n=size(nums);
-        for(int l=0,r=0;r<n;r++){
-            fo[nums[r]].push(r);
-
-            //size of queue is frequency of element
-            //if frequency is K+1 remove one
-            if(fo[nums[r]].size()==k+1){
-                //here l(left) is starting point of window
-                //if first occurence is inside window update left (l)
-                if(fo[nums[r]].front()>=l){
-                    l=fo[nums[r]].front()+1;
-                }
-                fo[nums[r]].pop();
+        while(j<nums.size()){
+            mp[nums[j]]++;
+            while(mp[nums[j]]>k && i<=j){
+                mp[nums[i]]--;
+                i++;
             }
-            ans=max(ans,r-l+1);
+            ans=max(ans,j-i+1);
+            j++;
         }
         return ans;
+
+        
     }
 };
+
+// class Solution {
+// public:
+//     int maxSubarrayLength(vector<int>& nums, int k) {
+//         int ans=0;
+//         //fo = first occurence
+//         unordered_map<int,queue<int>>fo;//store index of nums
+//         int n=size(nums);
+//         for(int l=0,r=0;r<n;r++){
+//             fo[nums[r]].push(r);
+
+//             //size of queue is frequency of element
+//             //if frequency is K+1 remove one
+//             if(fo[nums[r]].size()==k+1){
+//                 //here l(left) is starting point of window
+//                 //if first occurence is inside window update left (l)
+//                 if(fo[nums[r]].front()>=l){
+//                     l=fo[nums[r]].front()+1;
+//                 }
+//                 fo[nums[r]].pop();
+//             }
+//             ans=max(ans,r-l+1);
+//         }
+//         return ans;
+//     }
+// };
