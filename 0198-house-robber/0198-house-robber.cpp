633d75eb9b598782e3ae5bc0cac5f95adda4370f
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-05-20 00:19:24 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
+    int maximumRobbing(vector<int>& nums,int index,vector<int>& dp){
+        if(index==0) return nums[index];
+        if(index<0) return 0;
+        //check if previously present 
+        if(dp[index]!=-1) return dp[index];
+        //check for pick and non pick
+        int pick=  nums[index]+ maximumRobbing(nums, index-2,dp);
+        int notPick = 0+ maximumRobbing(nums, index-1, dp);
+
+        // return maximum to that index 
+        return dp[index]= max(pick, notPick);
+    }
     int rob(vector<int>& nums) {
-         int n = nums.size();
-        
-        int prev = nums[0];
-        int prev2 = 0;
+        vector<int>dp(nums.size()+1 , -1);
+       return  maximumRobbing(nums, nums.size()-1, dp);
 
-        for( int i=1 ; i<n ; i++ ){
-            
-            int pick = nums[i];
-            if( i>1 ) pick += prev2 ;
-
-            int notPick = prev ;  
-
-            int curr = max( pick, notPick );
-            prev2 = prev ;
-            prev = curr ;
-        }
-
-        return prev ;
+       
     }
 };
