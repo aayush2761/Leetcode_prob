6b198dfdfb6aa99a267ca2e843d651807cb1b9c2
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-05-23 02:43:06 +0530

    Time: 6 ms (19.65%), Space: 10.3 MB (32.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -20,27 +20,60 @@
 
 
 
+// ********Memoization***************
+
+
+// class Solution {
+// public:
+//      int totalPath(int i, int j , vector<vector<int>>&dp , vector<vector<int>>& obstacleGrid){
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
+       
+//         if(dp[i][j]!=-1) return dp[i][j];
+
+//         int up= totalPath(i-1,j,dp,obstacleGrid);
+//         int left= totalPath(i,j-1,dp,obstacleGrid);
+//         return dp[i][j]= up+ left;
+
+//     } 
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//        int m = obstacleGrid.size();
+//         int n= obstacleGrid[0].size();
+//         // if(m==1 && n==1 && obstacleGrid[0][0]==1) return 0;
+//         //   if(m==1 && n==1 && obstacleGrid[0][0]==0) return 1;
+//         vector<vector<int>>dp(m, vector<int>(n,-1));
+//          return totalPath(m-1,n-1,dp,obstacleGrid);
+//     }
+// };
+
+
 
 class Solution {
 public:
-     int totalPath(int i, int j , vector<vector<int>>&dp , vector<vector<int>>& obstacleGrid){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-       
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int up= totalPath(i-1,j,dp,obstacleGrid);
-        int left= totalPath(i,j-1,dp,obstacleGrid);
-        return dp[i][j]= up+ left;
-
-    } 
+     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
        int m = obstacleGrid.size();
         int n= obstacleGrid[0].size();
         // if(m==1 && n==1 && obstacleGrid[0][0]==1) return 0;
         //   if(m==1 && n==1 && obstacleGrid[0][0]==0) return 1;
         vector<vector<int>>dp(m, vector<int>(n,-1));
-         return totalPath(m-1,n-1,dp,obstacleGrid);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                 if(i >= 0 && j >= 0 && obstacleGrid[i][j]==1) {dp[i][j] = 0;; continue;}
+                  if(i==0 && j==0 ) {
+                     dp[i][j] = 1; 
+                    continue;
+                    }
+                 
+                    int up=0,left=0;
+                    if(i>0)  up= dp[i-1][j];
+                    if(j>0) left= dp[i][j-1];
+                    dp[i][j]= up+left;
+                 
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
