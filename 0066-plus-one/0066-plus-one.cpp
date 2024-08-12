67f666152e3778b2983cb35a75368103f8890847
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-08-13 03:30:09 +0530

    Time: 0 ms (100%), Space: 10.6 MB (26.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& d) {
-        int e=d.size()-1;
-        for(int i=e;i>=0;i--){
-            if(d[i]!=9){
-                d[i]++;
-                return d;
-            }
-            else{
-                d[i]= 0;
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> res;
+        int n = digits.size();
+        int carry = 1; // Start with carry as 1, since we are adding one to the number
 
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            if (sum == 10) {
+                res.push_back(0);
+                carry = 1; // Carry over to the next significant digit
+            } else {
+                res.push_back(sum);
+                carry = 0; // No carry needed anymore
             }
         }
-       d.insert(d.begin(),1);
-        return d;
-       }
+
+        if (carry > 0) res.push_back(carry); // Add carry if any left after the loop
+
+        reverse(res.begin(), res.end()); // Reverse to get the correct order
+        return res;
+    }
 };
