ffb71b534b897a5fa8ab043aedc0410163d7589b
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-09-16 00:42:23 +0530

    Time: 18 ms (82.11%), Space: 31.4 MB (70.81%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int k= goal;
-        int ans=0,sum=0;
-        unordered_map<int,int>mp;
-       
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(sum==k) ans++;
-            if(mp.find(sum-k)!=mp.end())
-                ans+=mp[sum-k];
-            mp[sum]++;
+    int sliding(vector<int>& nums, int goal){
+        int count=0;
+        int i=0;
+        int j=0;
+        int temp=0;
+        while(i<nums.size()){
+            temp+= nums[i];
+            while(temp>goal && j<=i){
+                // count++;
+                temp-=nums[j];
+                j++;
+            }
+            count+= i-j+1;
+            i++;
         }
-        return ans;
+        return count;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return sliding(nums,goal)-sliding(nums,goal-1);
     }
 };
