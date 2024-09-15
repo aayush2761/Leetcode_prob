610aea7e303e32774ec803568d627b14638d2423
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-09-15 14:30:13 +0530

    Time: 0 ms (100%), Space: 7.6 MB (49.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void  solve(int n , int &count ){
-        if(n==0){
-            // count=max(temp,count);
-            count++;
-            return;
-        }
-        if(n<0) return;
-        // one 
-        solve(n-1,count);
-        solve(n-2,count);
+    int solve(int n, vector<int>& dp) {
+        if (n == 0) return 1; 
+        if (n < 0) return 0;  
 
+        if (dp[n] != -1) return dp[n]; 
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
     }
+
     int climbStairs(int n) {
-        int count=0;
-        solve(n,count);
-        return count;
+        vector<int> dp(n + 1, -1);  
+        return solve(n, dp);
     }
 };
