dfeac8eae41f2a94bfcc6e8d7dd1ca92d3f4f190
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-09-24 23:11:08 +0530

    Time: 10 ms (5.99%), Space: 13.2 MB (9.56%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // index, pair(char,freq)
-        int n=strs.size();
-        if(n==1) return strs[0];
-        // get length
-        int maxi=INT_MAX;
-        for(int i=0;i<n;i++){
-         if(strs[i].size()<=maxi) maxi=strs[i].size();
-       }
-       for(int i=0;i<n;i++){
-         if(strs[i]=="") return strs[i];
-       }
-        string res="";
-        for(int i=0;i<maxi;i++ ){
-            char temp= strs[0][i];
-            int j=1;
-            while(j<n){
-                if(strs[j][i] != temp){
-                    return res;
-                }
-                j++;
+        unordered_map<string, int> mp;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = "";
+            for (auto j : strs[i]) {
+                temp += j;
+                mp[temp]++;
             }
-            if(j==n) res+=temp;
         }
-        return res;
+
+        int res = 0;
+        string temp;
+
+        for (auto i : mp) {
+            if (i.second == strs.size() && i.first.size() > res) {
+                res = i.first.size();
+                temp = i.first;
+            }
+        }
+        if (res == 0) return "";
+        return temp;
     }
 };
