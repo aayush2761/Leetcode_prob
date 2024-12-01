a08d483b992134cd6b65072508a97dad38aa7472
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-12-01 18:11:12 +0530

    Time: 0 ms (100%), Space: 14.9 MB (10.05%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        // sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                if(arr[i]==arr[j]*2 || arr[i]*2==arr[j] ) return true;
-            }
+        unordered_map<double,int>mp;
+        for(auto i : arr){
+            if((mp.find((i*2.0))!=mp.end()|| (mp.find(((i*1.0)/2.0))!=mp.end()))) return true;
+            mp[(i*1.0)]++;
         }
         return false;
     }
