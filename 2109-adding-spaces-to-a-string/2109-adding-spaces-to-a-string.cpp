7d693040c5ed63d9935d8cec294dc26058760ff5
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2024-12-03 11:18:01 +0530

    Time: 23 ms (37.65%), Space: 86.5 MB (11.32%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -1,14 +1,18 @@
-
 class Solution {
 public:
-    string addSpaces(string s, vector<int>& spaces) {
+    string addSpaces(string s, vector<int>& v) {
         string res = "";
-        int temp = 0;
-        for (int i = 0; i < spaces.size(); i++) {
-            res += s.substr(temp, spaces[i] - temp) + " ";
-            temp = spaces[i];
+        int i = 0, index = 0, prev = 0;
+        
+        while (index < v.size()) {
+            string p = s.substr(i, v[index] - prev);
+            res += p + ' ';
+            i += v[index] - prev;
+            prev = v[index];
+            index++;
         }
-        res += s.substr(temp);
+        
+        res += s.substr(i);
         return res;
     }
 };
