612c2722cd627843c4a36bdf9c78dad086082f67
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-02-22 15:43:33 +0530

    Time: 62 ms (14.51%), Space: 78.4 MB (8.16%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
+    unordered_map<int,int>mp;
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        bool present[n + 2]; // Auxiliary array to mark the presence of positive integers
-        
-        // Initialize present array elements to false
-        for (int i = 0; i < n + 2; i++) {
-            present[i] = false;
+        mp.clear();
+        int low=INT_MIN;
+        for(auto &it : nums){
+            mp[it]++;
+            low=max(low,it);
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0 && nums[i] <= n) {
-                present[nums[i]] = true; // Mark the presence of positive integers in nums
-            }
+        for(int i=1;i<=low;i++){
+            if(mp.find(i)==mp.end()) return i;
         }
-        
-        for (int j = 1; j <= n + 1; j++) {
-            if (!present[j]) {
-                return j; // Return the first missing positive integer
-            }
-        }
-        
-        return n + 1; // If all positive integers from 1 to n are present, return n + 1
+        return low>0 ? low+1 : 1;
+
     }
 };
