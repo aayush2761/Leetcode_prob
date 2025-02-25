f4c8d6113b662443b9143c86564b6cf8cb3976fc
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-02-25 18:17:35 +0530

    Time: 74 ms (83.08%), Space: 56.3 MB (46.53%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,47 +1,24 @@
-// class Solution {
-// public:
-//     int subarraysWithKDistinct(vector<int>& nums, int k) {
-//          long long ans=0;
-//         unordered_map<long long,long long>mp;
-//         for(long long l=0,r=0;r<nums.size();r++){
-//             mp[nums[r]]++;
-//             if(mp.size()==k) ans++;
-//             if(r==nums.size()-1){
-//                 r=l;
-//                 l++;
-                
-//                 mp.clear();
-//             }
-//         }
-//         return ans;
-
-//     }
-// };
-
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans = 0;
-        unordered_map<int, int> count1, count2;
-        int left1 = 0, left2 = 0;
-        int distinct1 = 0, distinct2 = 0;
-
-        for (int right = 0; right < n; ++right) {
-            if (count1[nums[right]]++ == 0) distinct1++;
-            if (count2[nums[right]]++ == 0) distinct2++;
-
-            while (distinct1 > k) {
-                if (--count1[nums[left1++]] == 0) distinct1--;
+    int countSubarray(vector<int>&nums, int k){
+        int res=0;
+        int i=0;
+        int j=0;
+        int n= nums.size();
+        unordered_map<int,int>mp;
+        while(j<=i && i<n){
+            mp[nums[i]]++;
+            while(mp.size()>k){
+                mp[nums[j]]--;
+                if(mp[nums[j]]==0)mp.erase(nums[j]);
+                j++;
             }
-
-            while (distinct2 > k - 1) {
-                if (--count2[nums[left2++]] == 0) distinct2--;
-            }
-
-            ans += left2 - left1;
+            res+= (i-j+1);
+            i++;
         }
-
-        return ans;
+        return res;
+    }
+    int subarraysWithKDistinct(vector<int>& v, int k) {
+         return  countSubarray(v,k)-countSubarray(v,k-1);
     }
 };
