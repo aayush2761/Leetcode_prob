7ba57c5720e72a7aee3360ce0c0f6a55b47ff271
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-04-21 09:16:15 +0530

    Time: 4 ms (32.26%), Space: 112.6 MB (47.58%) - LeetHub

---
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& A, int lower, int upper) {
+        long sum = 0, mn = 0, mx = 0;
+        for (int n : A) {
+            sum += n;
+            mn = min(mn, sum);
+            mx =max(mx, sum);
+        }
+        return max(0L, mn - mx + upper - lower + 1);
+    }
+};
