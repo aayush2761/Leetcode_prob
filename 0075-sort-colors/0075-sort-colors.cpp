c90e06988d95a40df9c3d8886139a240dae31e33
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-05-17 14:05:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (100%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1) return ; 
-        unordered_map<int,int>mp;
-        for(auto i :  nums) mp[i]++;
-       int i=0;
-        while (mp[0]) {
-            mp[0]--;
-            nums[i]=0;
-            i++;
-        }
-        while (mp[1]) {
-            mp[1]--;
-            nums[i]=1;
-            i++;
-        }
-        while (mp[2]) {
-            mp[2]--;
-            nums[i]=2;
-            i++;
-        }
-
-
+        sort(nums.begin(),nums.end());
     }
 };
