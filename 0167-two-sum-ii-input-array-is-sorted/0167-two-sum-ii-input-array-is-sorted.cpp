d8e22ba2313fbe1537f68b83476a35d1af5a7950
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-05-17 22:47:25 +0530

    Time: 3 ms (9.74%), Space: 20.7 MB (5.1%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int bs(vector<int>& nums,int key){
-        int res=0;
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low +(high-low) /2;
-            if(nums[mid]==key)return mid+1;
-            else if (nums[mid]>key)high=mid-1;
-            else low=mid+1;
-        }
-        return -1;
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            int x= bs(nums,target-nums[i]);
-            if(x!=-1 && x!=i+1){
-                if(x>i+1)return {i+1,x};
-                else return {x,i+1};
-            }
+        for(int i = 0; i < nums.size(); i++) {
+            int find = target - nums[i];
+            auto index = [&]() -> vector<int> {
+                int low = 0, high = nums.size() - 1;
+                while(low <= high) {
+                    int mid = low + (high - low) / 2;
+                    if(nums[mid] == find && i != mid) return {i+1, mid+1};
+                    else if (nums[mid] > find) high=mid-1;
+                    else low=mid+1;
+                }
+                return {}; 
+            };
+            vector<int> res = index();
+            if(!res.empty()) return res;
         }
         return {};
     }
