1884a503864ab2158b3a7a6fb3c0b077275c8284
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-05-20 01:24:08 +0530

    Time: 0 ms (100%), Space: 17.1 MB (62.55%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    int evaluateExpression(char op, int a, int b) {
-        if (op == '+')
-            return a + b;
-        else if (op == '-')
-            return a - b;
-        else if (op == '*')
-            return a * b;
-        else if (op == '/')
-            return a / b;
-        return 0; 
-    }
-    
-    int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-        for (int i = 0; i < tokens.size(); i++) {
-            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
-                int b = st.top();
-                st.pop();
-                int a = st.top();
-                st.pop();
-                st.push(evaluateExpression(tokens[i][0], a, b));
-            } else {
-                st.push(stoi(tokens[i]));
-            }
+    int evalRPN(vector<string>& token) {
+        stack<int>st;
+        for(int i=0;i<token.size();i++){
+            string t= token[i];
+             if (t == "+" || t == "-" || t == "*" || t == "/"){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                if(t=="+") st.push(b+a);
+                else if(t=="-") st.push(b-a);
+                else if(t=="*") st.push(b*a);
+                else  st.push(b/a);
+             }
+             else st.push(stoi(t));
         }
         return st.top();
     }
