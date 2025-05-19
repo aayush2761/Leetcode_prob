34108c857093cc76ddcec87c484dee82396e8ed3
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-05-20 00:48:21 +0530

    Time: 84 ms (5.03%), Space: 23.2 MB (73.87%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,43 +1,24 @@
 class MinStack {
 public:
-vector<pair<int,int>>vec;
-int mini=INT_MAX;
-int index=-1;
-
+vector<int>st;
     MinStack() {
         
     }
     
     void push(int val) {
-        mini=min(mini,val);
-        vec.push_back({val,mini});
-        index++;
+        st.push_back(val);
     }
     
     void pop() {
-    
-        vec.pop_back();
+        st.pop_back();
     }
     
     int top() {
-        if(index<0) return -1;
-        auto i = vec.back();
-        return i.first;
-
+        int n = st.size();
+        return st[n-1];
     }
     
     int getMin() {
-        if(index<0) return -1;
-          auto i = vec.back();
-        return i.second;
+        return *min_element(st.begin(),st.end());
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
