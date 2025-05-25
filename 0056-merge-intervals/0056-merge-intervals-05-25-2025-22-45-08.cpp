e3eb344680ad7a0ada3958f7e88a61108410529b
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-05-25 22:45:08 +0530

    Time: 25 ms (8.16%), Space: 23.9 MB (59.33%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals-05-25-2025-22-45-08.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        map<int, int> line;
+        for(auto& i : intervals){
+            ++line[i[0]];
+            --line[i[1]];
+        }
+       
+        int count = 0;
+        vector<vector<int>> ans;
+        int start = 0;
+        for(auto& i : line){
+            // that means its a new start, store the start
+           if(count ==0){
+               start = i.first;
+           }
+           count += i.second;
+           // this mean interval ends, and we can push this as answer
+           if(count==0){
+               ans.push_back({start, i.first});
+           }
+        }
+        return ans;
+    }
+}; 
