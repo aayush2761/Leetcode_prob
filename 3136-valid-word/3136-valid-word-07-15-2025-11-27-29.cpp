94c5a1c119c5256407c748145936c9ce5341409c
Aayush Gupta <145021649+aayush2761@users.noreply.github.com>
2025-07-15 11:27:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.47%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word-07-15-2025-11-27-29.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if(n<3) return 0;
+        int vowel=0,consonen=0,num=0;
+        set<char>st{'a','e','i','o','u','A','E','I','O','U'};
+        for(auto it : s){
+            int x = it-'0';
+            if(x>=0 && x<=9) num++;
+            else if (st.count(it))vowel++;
+            else if((x>=65 && x<=90)|| (x>=97 || x<= 122))consonen++;
+           
+           if(it=='@'|| it=='#'|| it=='$') return 0;
+        }
+        if(vowel>0 && consonen>0 ) return 1;
+        return 0;
+    }
+};
